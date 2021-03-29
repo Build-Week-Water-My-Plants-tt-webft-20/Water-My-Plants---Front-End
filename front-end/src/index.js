772b8548396b5a20e2d49acab9d69c2c03e41bda
Kirk <kirkrobertsnyder@gmail.com>
2021-03-29 12:52:33 -0400

    setup redux

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,12 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
 import reportWebVitals from './reportWebVitals';
+import logger from 'redux-logger'
+import reducer from './reducers'
+
+const store = createStore(reducer, applyMiddleware(logger))
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
