8671639f798d3fa2f873266baff9252163f53d3f
krsnyder <kirkrobertsnyder@gmail.com>
2021-03-30 15:40:44 -0400

    Started building CRUD buttons for dashboard

--- front-end/src/components/Dashboard.js
+++ front-end/src/components/Dashboard.js
@@ -1,16 +1,26 @@
 import React, {useState, useEffect} from 'react'
 import PlantCard from './PlantCard'
 import plantData from '../plantData'
+import PlantForm from './PlantForm'
 import "../components/CSS/Dashboard.css";
 
 const initialList = []
 
 export default function Dashboard() {
   const [plantList, setPlantList] = useState(initialList)
+  const [displayForm, setDisplayForm] = useState(false)
 
   useEffect(() => {
     setPlantList(plantData)
   }, [])
+
+  const openForm = () => {
+    setDisplayForm(true)
+  }
+
+  const closeForm = () => {
+    setDisplayForm(false)
+  }
 
   return (
     <div className="dashboard">
@@ -20,6 +30,17 @@ export default function Dashboard() {
           return <PlantCard key={plant.id} plantDetails={plant}/>
         })}
       </div>
+
+      {!displayForm && <div className="button" onClick={openForm}>
+        Add Plant
+      </div>}
+
+      {displayForm && <div className="button" onClick={closeForm}>
+        Close
+      </div>}
+
+      {displayForm &&
+        <PlantForm />}
     </div>
   )
 }
--- front-end/src/components/PlantCard.js
+++ front-end/src/components/PlantCard.js
@@ -4,6 +4,14 @@ import "../components/CSS/Dashboard.css";
 export default function PlantCard(props) {
   const {species, nickname, diameter, frequency, image} = props.plantDetails
 
+  const editPlant = () => {
+
+  }
+
+  const deletePlant = () => {
+    
+  }
+
   return (
     <div className="plant-card">
       <div className="card-header">
@@ -13,7 +21,16 @@ export default function PlantCard(props) {
       <p>{frequency}</p>
       <p>{species}</p>
       <p>{diameter}</p>
+
+      <div className="crud-buttons">
+        <div className="button" onClick={editPlant}>
+          Edit
+        </div>
+
+        <div className="button" onClick={deletePlant}>
+          Delete
+        </div>
+      </div>
     </div>
   )
 }
-// test
