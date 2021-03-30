3ef96aa18d30c2149866acde1b904b01ee740bb4
Kirk <kirkrobertsnyder@gmail.com>
2021-03-30 13:11:33 -0400

    fixed routing

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,30 +5,27 @@ import Login from "./components/Login"
 import Signup from "./components/Signup"
 
 import { Route } from "react-router-dom";
-import PrivateRoute from './components/PrivateRoute'
-import PlantList from './components/PlantList'
-import AddPlant from './components/AddPlant'
+// import PrivateRoute from './components/PrivateRoute'
+import Dashboard from './components/Dashboard'
+import PlantForm from './components/PlantForm'
 
 function App() {
+
+  // Setting test token to display private routes
+  // localStorage.setItem("token", "2264574286");
+  
   return (
     <div className="App">
       <NavigationBar />
 
-      <PrivateRoute exact path="plant-list" component={PlantList} />
-      <PrivateRoute exact path="add-plant" component={AddPlant} />
+      {/* <PrivateRoute exact path="plant-list" component={Dashboard} />
+      <PrivateRoute exact path="add-plant" component={PlantForm} /> */}
       
-      <Route path="/">
-
-        <Landing />
-      </Route>
-      ,
-      <Route path="/login">
-        <Login />
-      </Route>
-      ,
-      <Route path="/signup">
-        <Signup />
-      </Route>
+      <Route path="/dashboard" component={Dashboard}/>
+      <Route path="/plant-form" component={PlantForm}/>
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup}/>
+      <Route exact path="/" component={Landing}/>
     </div>
   );
 }
