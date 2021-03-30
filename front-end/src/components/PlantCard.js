6c32b8fd3ee86c589185e1f8f300eeccbdbd0ee9
Kirk <kirkrobertsnyder@gmail.com>
2021-03-30 15:05:53 -0400

    updating data

--- front-end/src/components/PlantCard.js
+++ front-end/src/components/PlantCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import "../components/CSS/Dashboard.css";
 
 export default function PlantCard(props) {
-  const {type, species, nickname, diameter, frequency, image} = props.plantDetails
+  const {species, nickname, diameter, frequency, image} = props.plantDetails
 
   return (
     <div className="plant-card">
@@ -10,10 +10,9 @@ export default function PlantCard(props) {
        <h2>{nickname}</h2>
        <img src={image} alt={species}/>
       </div>
-      <p>{type}</p>
+      <p>{frequency}</p>
       <p>{species}</p>
       <p>{diameter}</p>
-      <p>{frequency}</p>
     </div>
   )
 }
