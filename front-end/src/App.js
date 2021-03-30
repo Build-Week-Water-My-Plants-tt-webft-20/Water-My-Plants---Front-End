4d5304a1e66ca888321514eef96717322087dee6
Kirk <kirkrobertsnyder@gmail.com>
2021-03-29 21:38:17 -0400

    Added routes and components for plantlist and addplant

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,11 +2,17 @@ import "./App.css";
 import NavigationBar from "./components/Nav.js";
 import Landing from "./components/Landing";
 import { Route } from "react-router-dom";
+import PrivateRoute from './components/PrivateRoute'
+import PlantList from './components/PlantList'
+import AddPlant from './components/AddPlant'
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
+      
+      <PrivateRoute exact path="plant-list" component={PlantList} />
+      <PrivateRoute exact path="add-plant" component={AddPlant} />
       
       <Route path="/">
         <Landing />
