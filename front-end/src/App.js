9993b169237d05cf66c56c72d015bd09785d61ab
krsnyder <kirkrobertsnyder@gmail.com>
2021-03-31 17:56:23 -0400

    Added private route for dashboard

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import NavigationBar from "./components/Nav";
 import Landing from "./components/Landing";
 import Login from "./components/Login"
 import Signup from "./components/Signup"
+import PrivateRoute from './components/PrivateRoute'
 
 import { Route } from "react-router-dom";
 // import PrivateRoute from './components/PrivateRoute'
@@ -14,10 +15,8 @@ function App() {
     <div className="App">
       <NavigationBar />
 
-      {/* <PrivateRoute exact path="plant-list" component={Dashboard} />
-      <PrivateRoute exact path="add-plant" component={PlantForm} /> */}
+       <PrivateRoute exact path="dashboard" component={Dashboard} />
       
-      <Route path="/dashboard" component={Dashboard}/>
       <Route path="/login" component={Login} />
       <Route path="/signup" component={Signup}/>
       <Route exact path="/" component={Landing}/>
